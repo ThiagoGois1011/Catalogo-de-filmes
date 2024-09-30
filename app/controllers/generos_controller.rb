aa47809069aa4000c2b9a01668bc383c9ec756b5
Thiago Gois <thiagogoes1011@gmail.com>
2024-09-30 10:21:09 -0300

    Finalizado a função de cadastro de genêros.

--- app/controllers/generos_controller.rb
+++ app/controllers/generos_controller.rb
@@ -4,5 +4,20 @@ class GenerosController < ApplicationController
   end
 
   def new
+    @novo_genero = Genero.new
+  end
+
+  def create
+    parametro_genero = params[:genero][:nome].capitalize
+    if !Genero.find_by(nome: parametro_genero)
+      @novo_genero = Genero.new(nome: parametro_genero)
+
+      if @novo_genero.save
+        return redirect_to generos_path
+      end
+
+    end
+
+    render :new
   end
 end
